4971fb09207362c769c93c58c4fc333acf4d77d0
realtimebyte <realtimebyte@gmail.com>
2024-07-20 15:49:28 -0700

    Update the Api

--- GloboTicket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ GloboTicket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Models.Mail;
+using GloboTicket.TicketManagement.Infrastructure.FileExport;
 using GloboTicket.TicketManagement.Infrastructure.Mail;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,7 @@ namespace GloboTicket.TicketManagement.Infrastructure
         {
             services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
 
+            services.AddTransient<ICsvExporter, CsvExporter>();
             services.AddTransient<IEmailService, EmailService>();
 
             return services;
